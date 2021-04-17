a514205ce2a987a87bf1d8e0baa0cb25bb30a581
night <night4deadgaming@gmail.com>
2021-04-17 11:56:25 +0200

    commit 17/04 : changes to Calculer.java and CalculerTest.java

---
+++ src/test/java/com/iut/as2021/metiers/CalculerTest.java
@@ -0,0 +1,34 @@
+package com.iut.as2021.metiers;
+
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculerTest extends TestCase {
+    private Calculer calcul;
+
+    @Before
+    public void setUp(){
+        calcul = new Calculer();
+    }
+
+    @Test
+    public void testCasAdditionSimple() throws Exception{
+        assertEquals(calcul.run("4 + 5"),9,1);
+    }
+
+    @Test
+    public void testCasSoustractionSimple() throws Exception{
+        assertEquals(calcul.run("4 - 5"),-1,1);
+    }
+
+    @Test
+    public void testCasMultiplicationSimple() throws Exception{
+        assertEquals(calcul.run("4 * 5"),20,1);
+    }
+
+    @Test
+    public void testCasDivisionSimple() throws Exception{
+        assertEquals(calcul.run("4 / 5"),0.8,1);
+    }
+}
