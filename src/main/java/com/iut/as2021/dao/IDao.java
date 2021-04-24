d2d5071d4c279d23a116f2df582fb09c19e57664
tramoy5u <floriantramoy@gmail.com>
2021-04-24 11:53:02 +0200

    update

---
+++ src/main/java/com/iut/as2021/dao/IDao.java
@@ -0,0 +1,8 @@
+package com.iut.as2021.dao;
+
+import java.util.List;
+
+public interface IDao {
+
+
+}
--- src/test/java/com/iut/as2021/metiers/ExpressionTest.java
+++ src/test/java/com/iut/as2021/metiers/ExpressionTest.java
@@ -8,29 +8,87 @@ import org.junit.Test;
 public class ExpressionTest {
 
     private Expression resultat;
+    private Expression resultat2;
+    private Expression resultat3;
 
     @Test
-    public void testExpressionSimpleAddition() throws MathsExceptions {
-        resultat = new Expression("3+4+5+6");
-        Assert.assertEquals(resultat.calculate(), 18, 2);
+    public void testExpressionSimpleAdditionMembresPositifs() throws MathsExceptions {
+        resultat = new Expression("3+4");
+        Assert.assertEquals(resultat.calculate(), 7, 2);
+    }
+    @Test
+    public void testExpressionSimpleAdditionMembresNulls() throws MathsExceptions {
+        resultat = new Expression("0+0");
+        Assert.assertEquals(resultat.calculate(), 0, 2);
+    }
+    @Test
+    public void testExpressionSimpleAdditionPremierMembreNull() throws MathsExceptions {
+        resultat = new Expression("0+4");
+        Assert.assertEquals(resultat.calculate(), 4, 2);
     }
 
     @Test
-    public void testExpressionSimpleSoustraction() throws MathsExceptions {
-        resultat = new Expression("1+2-3+3");
-        Assert.assertEquals(resultat.calculate(), 3, 2);
+    public void testExpressionSimpleAdditionDeuxiemeMembresNull() throws MathsExceptions {
+        resultat = new Expression("4+0");
+        Assert.assertEquals(resultat.calculate(), 4, 2);
+    }
+    @Test
+    public void testExpressionSimpleSoustractionMembresPositifs() throws MathsExceptions {
+        resultat = new Expression("3-4");
+        Assert.assertEquals(resultat.calculate(), -1, 2);
+    }
+    @Test
+    public void testExpressionSimpleSoustractionMembresNulls() throws MathsExceptions {
+        resultat = new Expression("0-0");
+        Assert.assertEquals(resultat.calculate(), 0, 2);
+    }
+    @Test
+    public void testExpressionSimpleSoustractionPremierMembreNull() throws MathsExceptions {
+        resultat = new Expression("0-4");
+        Assert.assertEquals(resultat.calculate(), -4, 2);
     }
 
     @Test
-    public void testExpressionSimpleMultiplication() throws MathsExceptions {
-        resultat = new Expression("7*3");
-        Assert.assertEquals(resultat.calculate(), 21, 2);
+    public void testExpressionSimpleSoustractionDeuxiemeMembresNull() throws MathsExceptions {
+        resultat = new Expression("4-0");
+        Assert.assertEquals(resultat.calculate(), 0, 2);
     }
 
     @Test
-    public void testExpressionSimpleDivision() throws MathsExceptions {
+    public void testExpressionSimpleMultiplicationMembresPositifs() throws MathsExceptions {
+        resultat = new Expression("7*3*2");
+        Assert.assertEquals(resultat.calculate(), 42, 2);
+    }
+    @Test
+    public void testExpressionSimpleMultiplicationMembresNegatifs() throws MathsExceptions {
+        resultat = new Expression("-7*-3");
+        Assert.assertEquals(resultat.calculate(), 42, 2);
+        resultat2 = new Expression("-7*3");
+        Assert.assertEquals(resultat2.calculate(), 42, 2);
+        resultat3 = new Expression("3*(-7)");
+        Assert.assertEquals(resultat3.calculate(), 42, 2);
+    }
+
+    @Test
+    public void testExpressionSimpleMultiplicationMembreNull() throws MathsExceptions {
+        resultat = new Expression("0*4*5");
+        Assert.assertEquals(resultat.calculate(), 0, 2);
+        resultat2 = new Expression("4*0*5");
+        Assert.assertEquals(resultat2.calculate(), 0, 2);
+        resultat3 = new Expression("4*5*0");
+        Assert.assertEquals(resultat3.calculate(), 0, 2);
+    }
+
+
+    @Test
+    public void testExpressionSimpleDivisionMembresPositifs() throws MathsExceptions {
         resultat = new Expression("8 / 2");
         Assert.assertEquals(resultat.calculate(), 4, 2);
     }
 
+    @Test
+    public void testExpressionSimpleDivisionMembresNegatifs() throws MathsExceptions {
+        resultat = new Expression("-8 / -2");
+        Assert.assertEquals(resultat.calculate(), 4, 2);
+    }
 }
